162e74ae3c8cdae12e28d1a4e733022791b85e3e
Michel Lieben <liebenmichel@gmail.com>
2017-11-22 11:01:13 +0000

    vite fait

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,9 @@
 class PagesController < ApplicationController
-<<<<<<< HEAD
-<<<<<<< HEAD
-  skip_before_action :authenticate_user!, only: [ :home ]
+ skip_before_action :authenticate_user!, only: [:home, :confirmation]
 
   def home
-=======
-  skip_before_action :authenticate_user!, only: :confirmation
-  def confirmation
->>>>>>> bf095a276ad2de5eb023efe7643ac53ccc50cc85
-=======
- skip_before_action :authenticate_user!, only: [:home , :confirmation]
-    
-  def home
-  end 
+  end
 
   def confirmation
->>>>>>> 66d67a9e634500ba9c0fe64e68626afe9db00f71
   end
 end
