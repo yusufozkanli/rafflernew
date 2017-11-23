b26796cba8683b49c58cdf1e1e1697afdb26b1dd
Aurelien Alexandre <aurelien.alexandre@gmail.com>
2017-11-23 14:28:13 +0000

    launched browse page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'raffles#index'
+  get '/browse' , to: 'raffles#browse'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :raffles do
     resources :orders, only: [:new, :create,] do
