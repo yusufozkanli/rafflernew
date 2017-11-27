1c21a9c782a909ccdf5dabb76838b644032c5432
Ehsun Tafsiri <ehsuntafsiri@gmail.com>
2017-11-27 12:39:22 +0000

    updated dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
+require 'pry'
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_user
 
   def show
+    raffles_to_display = []
+
+    if params[:tab] == "participant"
+      @user.orders.each do |order|
+        order.raffle.user_id != @user.id ? raffles_to_display << order.raffle : nil
+      end
+    elsif params[:tab] == "organiser"
+      @user.orders.each do |order|
+        order.raffle.user_id == @user.id ? raffles_to_display << order.raffle : nil
+        end
+    end
+    @raffles = raffles_to_display
   end
+
 
   def edit
   end
