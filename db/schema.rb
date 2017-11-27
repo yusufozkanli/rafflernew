30258c621c32b59e91d44f301c5c1affa1163ba0
Michel Lieben <liebenmichel@gmail.com>
2017-11-27 18:24:37 +0000

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127161918) do
+ActiveRecord::Schema.define(version: 20171127175425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,12 +45,15 @@ ActiveRecord::Schema.define(version: 20171127161918) do
     t.string "color"
     t.text "description"
     t.datetime "end_date"
+    t.integer "max_ticket"
+    t.integer "min_ticket"
     t.string "status", default: "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
     t.integer "ticket_quantity"
     t.string "title"
+    t.integer "price_pennies", default: 0, null: false
     t.integer "available_tickets"
     t.index ["user_id"], name: "index_raffles_on_user_id"
   end
