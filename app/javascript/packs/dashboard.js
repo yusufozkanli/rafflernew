27b92d078954b2343c6c7928acd31acad8a18807
Ehsun Tafsiri <ehsuntafsiri@gmail.com>
2017-11-28 17:24:47 +0000

    created pending and active

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -1,9 +1,2 @@
-console.log('Hello world')
-
-const participant_link = document.getElementById("participant_tab");
-participant_link.addEventListener("click", (event) => {
-  console.log('Tab clicked')
-  // Get the user's tickets where the raffle.user != the current user
-});
 
 
