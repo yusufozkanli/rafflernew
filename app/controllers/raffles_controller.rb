258f79c3fb09bb7b17c8a8b935f3090d82711382
yusuf ozkanli <myozkanli@gmail.com>
2017-12-01 11:15:25 +0000

    dont merge

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -3,7 +3,7 @@ class RafflesController < ApplicationController
 
   def index
     @raffles = Raffle.where("status = 'active'").last(4)
-    @raffles_ending = Raffle.where("status = 'active'" && "end_date >= '#{DateTime.now}'").order(:end_date).first(4)
+    @raffles_ending = Raffle.where("status = 'active'", "end_date >= '#{DateTime.now}'", "available_tickets > 0").order(:end_date).first(4)
     won_orders = Order.where(won: true).last(4)
     @orders = won_orders.last(4)
   end
