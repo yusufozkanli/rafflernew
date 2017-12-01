0efa6582745c7be0d623ffa0a4e73e344fbc177b
Michel Lieben <liebenmichel@gmail.com>
2017-12-01 15:27:25 +0000

    done

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :raffle
   belongs_to :user
-  has_one :command, :dependent :destroy
+  has_one :command, dependent: :destroy
   monetize :price_pennies
 end
