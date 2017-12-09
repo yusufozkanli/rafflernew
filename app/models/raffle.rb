6f222bcc22514c7aac8ed35ffc85b7cac3a46c63
Michel Lieben <liebenmichel@gmail.com>
2017-12-09 16:46:15 +0100

    remove blue link and underline on lins

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -2,8 +2,8 @@ class Raffle < ApplicationRecord
   belongs_to :user
   has_many :orders, dependent: :destroy
   validates :title, :description, :end_date, :ticket_quantity, presence: true
-  after_create :set_winner_draw_job
-  after_update :set_winner_draw_job
+  # after_create :set_winner_draw_job
+  # after_update :set_winner_draw_job
   mount_uploader :photo, PhotoUploader
 
   def time_left_in_seconds
@@ -15,10 +15,10 @@ class Raffle < ApplicationRecord
   CATEGORIES = ['Electronics','Cars','Fashion','Jewelery', 'Experiences', 'Other']
 
 
-  def set_winner_draw_job
-    if self.status == "active"
-      PickWinnerJob.set(wait_until: self.end_date).perform_later(self)
-    end
-  end
+  # def set_winner_draw_job
+  #   if self.status == "active"
+  #     PickWinnerJob.set(wait_until: self.end_date).perform_later(self)
+  #   end
+  # end
 
 end
