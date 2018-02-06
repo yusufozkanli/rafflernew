386d5a73d62c944810d957a652bf24ca569acc32
Michel Lieben <liebenmichel@gmail.com>
2018-02-06 17:28:22 +0100

    newsletter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105010418) do
+ActiveRecord::Schema.define(version: 20180206155542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(version: 20180105010418) do
     t.integer "price_cents", default: 0, null: false
     t.integer "reserve_ticket_quantity"
     t.index ["user_id"], name: "index_raffles_on_user_id"
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
